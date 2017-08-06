49227c4933653bd6f64214f9989fd21e4726a87a
Michael Archibald <micharch54@gmail.com>
2017-08-06 10:26:18 -0600

    Got the random questions working correctly

--- quizzer.rb
+++ quizzer.rb
@@ -8,74 +8,51 @@ end
 
 # This application has too much state. I need to figure out how to shrink that
 
-question_count = ARGV[0]
+question_count  = ARGV[0]
 @used_questions = []
-@strands = []
-@standards = []
-@question_ids = []
-raise(QuestionCountError.new("Question Count must be a positive integer, got #{question_count}")) unless question_count.to_i > 0
+@strands        = []
+@standards      = []
+@question_ids   = []
+unless question_count.to_i > 0
+  raise(QuestionCountError.new,
+        "Question Count must be a positive integer, got #{question_count}")
+end
 
-questions = []
+@questions = []
 CSV.foreach('questions.csv', headers: true) do |row|
-  @strands << row["strand_id"].to_i
-  @standards << row["standard_id"].to_i
-  @used_questions << row["question_id"].to_i
-  questions << {
-      strand_id:   row["strand_id"],
-      strand_name: row["strand_name"],
-      standard_id:       row["standard_id"],
-      standard_name:     row["standard_name"],
-      question_id:         row["question_id"],
-      question_difficulty: row["difficulty"]
+  @strands << row['strand_id'].to_i
+  @standards << row['standard_id'].to_i
+  @used_questions << row['question_id'].to_i
+  @questions << {
+    strand_id:           row['strand_id'],
+    strand_name:         row['strand_name'],
+    standard_id:         row['standard_id'],
+    standard_name:       row['standard_name'],
+    question_id:         row['question_id'],
+    question_difficulty: row['difficulty']
   }
 end
 
-def frequency(obj)
-  obj.inject(Hash.new(0)) { |h,v| h[v.to_i] += 1; h }
-end
-
-def lowest(arr)
-  return 1 if arr.empty?
-  freq = frequency(arr)
-  arr.min_by { |v| freq[v].to_i }.to_i
-end
-
-def lowest_strand
-  lowest(@strands)
-end
-
-def lowest_standard
-  lowest(@standards)
-end
-
-def lowest_question
-  lowest(@used_questions)
-end
-
-# My logic is flawed here, there's obviously cases where the lowest of each won't exist in the array, I should
-# be looking through the questions and finding the strands that haven't been used, then checking for the standards
-# then checking the questions.
 def add_question(output, questions)
-  question_to_add = questions.find do |question|
-    question[:strand_id].to_i == lowest_strand &&
-        question.dig(:standard_id).to_i == lowest_standard &&
-        question.dig(:question_id).to_i == lowest_question
+  question = questions.sample(1)[0]
+  unless @used_questions.include?(question)
+    @used_questions << question
+    output << question
   end
-  return if question_to_add.nil?
-  output << question_to_add
-  @strands << question_to_add[:strands_id]
-  @standards << question_to_add.dig(:standard_id)
-  @used_questions << question_to_add.dig(:question_id)
 end
 
 output = []
-question_count.to_i.times do
-  add_question(output, questions)
+while output.length < question_count.to_i do
+  if @used_questions.length == @questions.length
+    @used_questions = []
+  end
+  add_question(output, @questions)
 end
-puts output.map{|o| o[:question_id]}
-CSV.open("./usage.csv", "wb") do |csv|
-  csv << ["question_id"]
-  output.each {|o| csv << [o[:question_id]]}
+
+puts "#{output.map { |o| o[:question_id] }.join(',')}"
+CSV.open('./usage.csv', 'wb') do |csv|
+  csv << ['question_id']
+  output.each { |question| csv << [question[:question_id]] }
 end
 
 
